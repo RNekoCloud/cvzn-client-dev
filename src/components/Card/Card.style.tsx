aa99a7125d8fc5afd92f1848427765a7177dff9a
RageNeko26 <rneko2006@gmail.com>
2023-08-30 11:24:39 +0700

    fix: Styling in input

--- src/components/Card/Card.style.tsx
+++ src/components/Card/Card.style.tsx
@@ -9,8 +9,7 @@ export const CardContainer = styled.div`
 `
 
 export const Card = styled.div`
-    max-width: 25%;
-    overflow: hidden;
+    width: 25%;
     height: 70vh;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     padding: 10px;
--- src/components/Input/Input.style.tsx
+++ src/components/Input/Input.style.tsx
@@ -1,13 +1,18 @@
 import { styled } from "styled-components"
 
 export const InputTitle = styled.label`
-    color: #61677A;
+    color: #D8D9DA;
     margin: 8px;
+    font-size: 15px;
 `
 
 export const InputText = styled.input`
-    width: 100%;
-    padding: 10px;
+    max-width: 100%;
+    min-width: 80%;
+    margin: 8px;
+    display: block;
+    padding: 5px;
     border: none;
     outline: none;
+    border-bottom: 1px solid #D8D9DA;
 `
