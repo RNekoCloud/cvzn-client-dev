e2b40dd77066c2f302dbaa9aec20fdd6209e4f54
RageNeko26 <rneko2006@gmail.com>
2023-08-30 23:06:58 +0700

    feat: Listening server

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import express, { Express, Request, Response } from "express"
 const path = require("path")
 
 const app: Express = express()
+const PORT: number = 5000
 
 app.use("/", express.static(path.join(__dirname, "public")))
 
@@ -11,4 +12,12 @@ app.get("/api/v1/", (req: Request, res: Response) => {
         message: "Welcome to sample API",
         success: true,
     })
+})
+
+app.get("/*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
 })
