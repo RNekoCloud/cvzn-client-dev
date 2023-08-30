65ff80a34e96cc5cc6e6d0c512850be94f6ef907
RageNeko26 <rneko2006@gmail.com>
2023-08-30 17:35:10 +0700

    fix: remove unused state

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,31 +1,21 @@
 import axios from "axios"
-import { useState, useRef } from "react"
+import { useRef } from "react"
 import { CardContainer, Card, CardTitle } from "../components/Card/Card.style"
 import { Button } from "../components/Button/Button.style"
-import { Auth } from "../types/payload"
 
 const Register = () => {
     const emailInput = useRef<any>()
     const passwordInput = useRef<any>()
 
-    const [authState, setAuthState] = useState<Auth>({
-        email: "",
-        password: "",
-    })
 
     const handler = async() => {
         let emailVal = emailInput.current.value
         let passwordVal = passwordInput.current.value
 
-        setAuthState({
-            email: emailVal,
-            password: passwordVal,
-        })
-
         try {
            const res = axios.post("http://ec2-3-113-2-184.ap-northeast-1.compute.amazonaws.com/api/v1/auth/signup", {
-            email: authState.email,
-            password: authState.password,
+            email: emailVal,
+            password: passwordVal,
            })
 
            console.log(res)
