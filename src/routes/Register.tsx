14d081ac443a32fadd19d62b3c2386dbf59501f6
RageNeko26 <rneko2006@gmail.com>
2023-08-30 13:30:19 +0700

    feat: Add handler

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,9 +1,19 @@
 import { CardContainer, Card, CardTitle } from "../components/Card/Card.style"
 import { Button } from "../components/Button/Button.style"
+import { useState, useRef } from "react"
+import { Auth } from "../types/payload"
 
 const Register = () => {
+    const emailInput = useRef<any>()
+    const passwordInput = useRef<any>()
+
+    const [authState, setAuthState] = useState<Auth>({
+        email: "",
+        password: "",
+    })
+
     const handler = () => {
-        console.log("yeaaa")
+        console.log("Testing...")
     }
 
     return (
@@ -12,9 +22,9 @@ const Register = () => {
                 <Card>
                     <CardTitle>Register</CardTitle>
                     <label htmlFor="" className="form__label">Email</label>
-                    <input type="text" className="form__input" />
+                    <input type="text" className="form__input" ref={emailInput} />
                     <label htmlFor="" className="form__label">Password</label>
-                    <input type="password" className="form__input" />
+                    <input type="password" className="form__input" ref={passwordInput} />
                     <label htmlFor="" className="form__label">Password Confirmation</label>
                     <input type="password" className="form__input" />
                     <Button onClick={handler}>Register</Button>
