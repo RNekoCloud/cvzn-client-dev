281fadd43be26c2080e86ec45954af13ca64dd99
RageNeko26 <rneko2006@gmail.com>
2023-09-02 00:12:09 +0700

    feat: Add styled error message

--- src/components/Popup/Popup.style.ts
+++ src/components/Popup/Popup.style.ts
@@ -1,12 +1,15 @@
 import { styled } from "styled-components"
 
 export const PopupWrapper = styled.div`
-    padding: 12px;
+    padding: 5px;
+    margin: 5px;
     border: 1px solid red;
 `
 
 export const PopupMsg = styled.p`
     color: red;
+    font-size: 12px;
+    margin: 5px;
 `
 
 
