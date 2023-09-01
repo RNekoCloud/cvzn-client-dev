d5342dcf8b0da11ef45ef4ae438a79774762a3ee
RageNeko26 <rneko2006@gmail.com>
2023-09-02 00:04:45 +0700

    feat: Add popup component

---
+++ src/components/Popup/Popup.style.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+export const PopupWrapper = styled.div`
+    padding: 12px;
+    border: 1px solid red;
+`
+
+export const PopupMsg = styled.p`
+    color: red;
+`
+
+
+
