10461ad85b8d34ae219bb31f7fa24ecc65f1a2fd
RageNeko26 <rneko2006@gmail.com>
2023-09-22 17:07:54 +0700

    fix: Add props styling

--- src/components/Popup/Popup.style.ts
+++ src/components/Popup/Popup.style.ts
@@ -3,11 +3,10 @@ import { styled } from "styled-components"
 export const PopupWrapper = styled.div`
     padding: 5px;
     margin: 5px;
-    border: 1px solid red;
 `
 
-export const PopupMsg = styled.p`
-    color: red;
+export const PopupMsg = styled.p<{$success?: boolean}>`
+    color: ${props => props.$success ? "#A8DF8E" : "red"};
     font-size: 13px;
     margin: 5px;
 `
