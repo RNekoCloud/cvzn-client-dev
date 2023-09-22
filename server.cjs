be53084debcdd487e02ca6da6755e99b39db8f99
RageNeko26 <rneko2006@gmail.com>
2023-09-22 15:12:48 +0700

    chore: Handling JSON Payload

--- server.cjs
+++ server.cjs
@@ -1,9 +1,24 @@
 const express = require("express")
-const path = require("path")
+const bodyParser = require("body-parser")
 
 const app = express()
 
 app.use("*", express.static(__dirname))
+app.use(bodyParser.json())
+
+
+app.post("/api/v1/auth/signup", (req, res) => {
+    res.json({
+      message: "Successfully adding data",
+      email: req.body.email
+    }) 
+})
+
+app.get("/redirect", (req, res) => {
+  res.json({
+    message: "After authentication"
+  })
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
