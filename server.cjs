5be93a15c65769a956156782615af4386f449a8d
RageNeko26 <rneko2006@gmail.com>
2023-09-22 17:00:49 +0700

    fix: Handling error in HTTP calls microservice

--- server.cjs
+++ server.cjs
@@ -2,7 +2,6 @@ const axios = require("axios")
 const express = require("express")
 const bodyParser = require("body-parser")
 
-
 const app = express()
 
 app.use("*", express.static(__dirname))
@@ -10,17 +9,26 @@ app.use(bodyParser.json())
 
 
 app.post("/api/v1/auth/signup", async(req, res) => {
-
-  const register = await axios.post("http://localhost:8080/api/v1/auth/signup", {
-    email: req.body.email,
-    password: req.body.password
+  try {
+    const register = await axios.post("http://localhost:8080/api/v1/auth/signup", {
+      email: req.body.email,
+      password: req.body.password
   })
 
   res.json({
       message: register.data.message,
       status: register.data.status
     })
+ 
+  } catch(err) {
+    console.log(err)
+    res.json({
+      message: register.data.message,
+      status: register.data.status
+    })
+  }
 
+ 
     
 })
 
